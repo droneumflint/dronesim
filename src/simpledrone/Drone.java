35fc6795097701b2c6d91449c774076b3a4da655
nicster34 <nicster34@gmail.com>
2016-11-30 18:15:58 -0500

    Added Grid motion & Battery range.

--- src/simpledrone/Drone.java
+++ src/simpledrone/Drone.java
@@ -1,11 +1,10 @@
 package simpledrone;
 
-import java.awt.Color;
 import sim.engine.*;
 import sim.util.*;
 import sim.field.continuous.*;
 import sim.portrayal.simple.OvalPortrayal2D;
-
+import java.awt.Color;
 
 @SuppressWarnings("serial")
 public class Drone implements Steppable{
@@ -14,14 +13,24 @@ public class Drone implements Steppable{
 	Continuous2D yard; //Yard at Start of Step
 	public OvalPortrayal2D myPortrayal2D = new OvalPortrayal2D(Color.GREEN); //Default Portrayal
 	double batteryPercent = 75; 
-	double batteryDrainRate = 1;//Battery drain for distance traveled
-	final double droneSpeed = .5; //Distance drone can move in one step 
-	private double cellRange = 25;//Distance drone can be away from Base and communicate
+	double batteryDrainRate = .05;//Battery drain for each distance traveled
+	final double droneSpeed = 5; //Distance drone can move in one step 
+	private double cellRange = 50;//Distance drone can be away from Base and communicate
 	private double wifiRange = 10;//Distance drone can be used as relay to communicate to Base
 	public int droneId;// Declared when initialized in Environment
+	Double2D[] points = new Double2D[8];
+	int help=0;
+	boolean isBoundaries;
 	
 	public Drone (int droneNumber){
 		this.droneId = droneNumber;
+		for (int i=0;i<8; i++){
+			if((i%2)==0){
+				points[i]= new Double2D((droneId*5)+i*10,0);
+			}else{
+				points[i]= new Double2D((droneId*5)+i*10,80);
+			}			
+		}
 	};
 	
 	//Called each Step in simulator
@@ -29,7 +38,16 @@ public class Drone implements Steppable{
 		environment = (Environment) state;
 		yard = environment.yard;
 		myPosition = environment.yard.getObjectLocation(this);
-		moveToPoint(new Double2D((droneId*34),droneId*34), true);
+		//moveToPoint(new Double2D((droneId*34),droneId*34), true);
+		if(inBatteryRange()){
+			moveInGrid();
+		}
+		else{
+			moveToPoint(environment.baseLocation, isBoundaries);
+			if(atPoint(environment.baseLocation, 2)){
+				rechargeBattery();
+			}
+		}
 		//if (hasAssist()){
 		//	environment.yardPortrayal.setPortrayalForObject(this, new OvalPortrayal2D(Color.GREEN));
 		//}else{			environment.yardPortrayal.setPortrayalForObject(this, new OvalPortrayal2D(Color.RED));}
@@ -49,10 +67,17 @@ public class Drone implements Steppable{
 			yard.setObjectLocation(this, temp);		
 		}
 	};
+	public void moveInGrid(){
+		int error = moveToPoint(points[help],isBoundaries);
+		if ((atPoint(points[help],1.0)  || error==1)&& help<7){
+			help++;
+		};
+	}
 
 	//Tries to move to point in yard, doesn't if point is out of bounds and isBoundaryOn is true
-	public void moveToPoint(Double2D point, boolean isBoundaryOn){
+	public int moveToPoint(Double2D point, boolean isBoundaryOn){
 		Double2D temp =myPosition.subtract(point);
+		int error = 0;
 		if(isBoundaryOn){
 			if (Math.abs(temp.length())>1){
 				if(temp.length()>droneSpeed){
@@ -61,20 +86,31 @@ public class Drone implements Steppable{
 				temp=temp.add(myPosition);
 				
 				if(inBounds(temp)){
-					batteryPercent -=(temp.length()*batteryDrainRate);
+					batteryPercent -=(batteryDrainRate*temp.length());
 					yard.setObjectLocation(this, temp);
-				}			
+					
+				}else{error++;}			
 			}
-		}else{
+		}
+		else{
 			if (Math.abs(temp.length())>1){
 				if(temp.length()>droneSpeed){
 					temp=temp.resize(-droneSpeed);
 				}
 				temp=temp.add(myPosition);
-				batteryPercent = batteryPercent-batteryDrainRate; 
-				yard.setObjectLocation(this, temp);		
+				batteryPercent -=(batteryDrainRate*temp.length()); 
+				yard.setObjectLocation(this, temp);	
 			}
 		}
+		return error;
+		
+	};
+	public void rechargeBattery(int chargeAdded){
+		batteryPercent += chargeAdded;
+		
+	};
+	public void rechargeBattery(){
+		batteryPercent =100;
 		
 	};
 	
@@ -92,7 +128,7 @@ public class Drone implements Steppable{
 		return myPosition.distance(environment.baseLocation)<((batteryPercent-batteryDrainRate)*droneSpeed);
 	};	
 	private boolean inBatteryRange(Double2D point) {
-		return point.distance(environment.baseLocation)<((batteryPercent-batteryDrainRate)*droneSpeed);
+		return point.distance(environment.baseLocation)<((batteryPercent)*droneSpeed);
 	};	
 	public boolean hasAssist() {
 		Bag inWifiRange = yard.getNeighborsWithinDistance(myPosition, wifiRange);
@@ -101,7 +137,8 @@ public class Drone implements Steppable{
 				inWifiRange.remove(i);
 			}
 		}
-		return (inWifiRange.numObjs>1);
+		//return (inWifiRange.numObjs>1);
+		return false;
 	};
 	private boolean hasAssist(Double2D point) {
 		Bag inWifiRange = yard.getNeighborsWithinDistance(point, wifiRange);
@@ -110,47 +147,19 @@ public class Drone implements Steppable{
 				inWifiRange.remove(i);
 			}
 		}
-		return (inWifiRange.numObjs>1);
+		//return (inWifiRange.numObjs>1);]
+		return false;
 	};
 	private boolean inBounds(Double2D point){
-		return ((hasAssist(point)||inCellRange(point))&& inBatteryRange(point));
+		return ((hasAssist(point)||inCellRange(point)));
+	};
+	private boolean inBounds(){
+		return ((hasAssist()||inCellRange()));
 	};
 
+	public void setBoundaries(boolean boundaries) {
+		this.isBoundaries = boundaries;
+	}
 }
-
-
-
-
-
-/*
-public double bxdir(double x){
-	//convert the SimState to our Environment
-	if(x<0 || x>= environment.gridHeight) {
-		xdir = -xdir; //reverse direction
-		return x+xdir ; //revert to previous
-	}
-	else {
-		return x;
-	}
-};
-public double bydir(double y){
-	//convert the SimState to our Environment
-	if(y<0 || y>= environment.gridHeight) {
-		ydir = -ydir; //reverse direction
-		return y + ydir; // revert to previous
-	}
-	else {
-		return y;
-	}
-};
-public void moveWithBoundaries(SimState state){
-	double x=myPosition.x,y=myPosition.y;
-	x = x + xdir; //move one step
-	y = y + ydir; //move one step
-	x = bxdir(x); //correct x location for boundaries
-	y = bydir(y);//correct y location for boundaries
-	yard.setObjectLocation(this, new Double2D(x,y));
-	//sets the particle in the new location.
-};*/
 
 
